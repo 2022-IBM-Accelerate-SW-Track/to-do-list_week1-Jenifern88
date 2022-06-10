30c8ca42a9edb68504ec37e2214bd698d781143d
Jenifern88 <jenifernguyen9196@yahoo.com>
2022-06-09 21:25:58 -0400

    about 2

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profilePic from "../assets/profile_pic_name.jpeg";
   
-export default class About extends Component {
+export default class About extends Component{
   render() {
     return (
       <div>
-        <p> </p> 
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src = {profile_pic_name}
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Jenifer Nguyen</div>
+          <div className="brief_description">
+            Student at George Mason University || Statistics
+          </div>
+        </div>
+      </div>
+    </div>
     )
+    }
   }
-}
